52251b42c2c1ea0ecd0d7d1e20deba2db4129f4d
NatanYirg <natiyirgish@gmail.com>
2023-09-29 18:50:13 +0000

    commit

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,17 @@
-import Building from './5-building';
+javascript
+import Building from './5-building.js';
 
-export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this.floors = floors;
-  }
+class SkyHighBuilding extends Building {
+	  constructor(sqft, floors) {
+		      super(sqft);
+		      this._floors = floors;
+		    }
 
-  get floors() {
-    return this._floors;
-  }
+	  get floors() {
+		      return this._floors;
+		    }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
-  }
+	  evacuationWarningMessage() {
+		      return `Evacuate slowly the ${this._floors} floors.`;
+		    }
 }
