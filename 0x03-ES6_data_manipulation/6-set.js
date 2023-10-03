3240c9f2421785ce2f0f12bb0ea0faca1885cb3f
NatanYirg <natiyirgish@gmail.com>
2023-10-03 20:09:45 +0000

    commit

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Nathan Yirgalem <https://github.com/NathanYirg>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
