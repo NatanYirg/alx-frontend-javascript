539a69845e3d06846531b9ce79aea478d1751cb3
NatanYirg <natiyirgish@gmail.com>
2023-10-04 17:55:24 +0000

    commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,32 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'larry',
+    lastName: 'lobster',
+    age: 678,
+    location: 'bikini bottom',
+}
+
+const student2: Student = {
+    firstName: 'mermaid man',
+    lastName: 'man',
+    age: 678,
+    location: 'the invisible boat mobile',
+}
+
+let studentsList: Student[] = [];
+
+studentsList.push(student1);
+studentsList.push(student2);
+
+let myVar = document.createElement('table');
+
+for (let i = 0; i < studentsList.length; i++) {
+    let row = myVar.insertRow();
+    let name = row.insertCell();
+    let location = row.insertCell();
